a56228c06f661afcf226532a73de2133d92bf7e4
Clint <wantg78@gmail.com>
2016-04-09 12:37:55 +0800

    no message

--- build/build.js
+++ build/build.js
@@ -4,27 +4,41 @@
 
 const os = require('os');
 const path = require('path');
+const util = require('util');
+const childProcess = require('child_process');
 const packager = require('electron-packager');
 
+const appTitle = 'Mustang';
+const appVersion = '1.1';
+const binPath = path.join(__dirname, './bin');
+const buildPath = __dirname;
 const platform = os.platform();
+var arch = 'x64';
+
+if (platform == 'win32') {
+    arch = 'ia32';
+}
 
 const opts = {
     'name': 'Mustang',
-    'arch': 'x64',
+    'arch': arch,
     'dir': path.join(__dirname, '../sources'),
     'platform': platform,
     // 'version': '0.36.9',
     'asar': true,
     'overwrite': true,
     'icon': path.join(__dirname, '../resources/icon.icns'),
-    'out': path.join(__dirname, './bin'),
+    'out': binPath,
 };
-
-if (platform == 'win32') {
-    opts.arch = 'ia32';
-}
 
 console.log(opts);
 packager(opts, function(err, appPath) {
-    console.log(err || appPath);
+    // console.log(err || appPath);
+    if (platform == 'darwin') {
+        childProcess.execSync(util.format('hdiutil create -volname %s-%s -srcfolder %s/%s-%s-%s/%s.app -ov -format UDZO %s/%s-%s-osx.dmg',
+            appTitle,appVersion,
+            binPath,appTitle,platform,arch,appTitle,
+            binPath,appTitle,appVersion));
+        childProcess.execSync(util.format('rm -rf %s/%s-%s-%s', binPath,appTitle,platform,arch));
+    }
 });
